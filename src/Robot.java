075f291e93aa2efb6cb80f33bac75701674186ed
Johannes Hofmann-Wellenhof <gianni.hw@gmail.com>
2011-12-07 00:32:46 +0100

    added comments

--- src/Robot.java
+++ src/Robot.java
@@ -1,34 +1,73 @@
-
+/**
+ * Abstract class Robot
+ * 
+ * @author Gruppe222
+ */
 public abstract class Robot {
 	protected final int number;
 	protected int operatingHours;
 	protected Usage use;
-	
+
+	/**
+	 * First constructor
+	 * 
+	 * @param nr
+	 *            number as integer
+	 */
 	protected Robot(int nr) {
 		this.number = nr;
 	}
-	
+
+	/**
+	 * Alternative constructor
+	 * 
+	 * @param nr
+	 *            number as integer
+	 * @param hour
+	 *            operatingHours as integer
+	 */
 	protected Robot(int nr, int hour) {
 		this.number = nr;
 		this.operatingHours = hour;
 	}
 
+	/**
+	 * Getter for Operating hours
+	 * 
+	 * @return hours as integer
+	 */
 	public int getOperatingHours() {
 		return this.operatingHours;
 	}
-	
+
+	/**
+	 * Increases the operating hours by 1.
+	 */
 	public void increaseOperatingHours() {
 		this.operatingHours++;
 	}
-	
+
+	/**
+	 * Changes the usage type of the robot
+	 * 
+	 * @param u
+	 */
 	public void changeType(Usage u) {
 		this.use = u;
 	}
-	public int getId(){
-		return number;		
+
+	/**
+	 * Getter for the number
+	 * 
+	 * @return the number as integer
+	 */
+	public int getId() {
+		return number;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#hashCode()
 	 */
 	@Override
@@ -40,18 +79,37 @@ public abstract class Robot {
 		return result;
 	}
 
-	public final boolean equal (Robot that) {
+	/**
+	 * equal method for two robots
+	 * 
+	 * @param that
+	 *            the other robot
+	 * @return true or false
+	 */
+	public final boolean equal(Robot that) {
 		if (this.getClass() == that.getClass())
 			return uncheckedEqual(that);
 		return false;
 	}
-	
+
+	/**
+	 * Abstract method should be implemented in subtypes.
+	 * 
+	 * @param r
+	 *            the other robot
+	 * @return true or false
+	 */
 	protected abstract boolean uncheckedEqual(Robot r);
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return "Robot:" + number + ", operatingHours=" + operatingHours
 				+ ", use=" + use;
 	}
-	
+
 }
