6584f0d950883415273a0b3a8c84f6dda97f072b
Johannes Hofmann-Wellenhof <gianni.hw@gmail.com>
2011-12-07 00:41:08 +0100

    added comments

--- src/CaterpillaRobot.java
+++ src/CaterpillaRobot.java
@@ -1,22 +1,56 @@
+/**
+ * Class CaterpillaRobot extends the class Robots
+ * 
+ * @author Gruppe222
+ * 
+ */
 public class CaterpillaRobot extends Robot {
 	private float distance;
 
+	/**
+	 * First constructor, calls the super constructors
+	 * 
+	 * @param nr
+	 *            the number of the robot
+	 */
 	public CaterpillaRobot(int nr) {
 		super(nr);
 	}
 
+	/**
+	 * Alternative constructor, calls the super constructor
+	 * 
+	 * @param nr
+	 *            the number of the robot
+	 * @param hour
+	 *            the operating hours
+	 */
 	public CaterpillaRobot(int nr, int hour) {
 		super(nr, hour);
 	}
 
+	/**
+	 * Getter for the distance
+	 * 
+	 * @return the distance as float
+	 */
 	public float getDistance() {
 		return this.distance;
 	}
 
+	/**
+	 * Increments the distance by 1.
+	 */
 	public void increaseDistance() {
 		this.distance++;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see Robot#uncheckedEqual(Robot)
+	 */
+	@Override
 	protected boolean uncheckedEqual(Robot r) {
 		return ((CaterpillaRobot) r).distance == this.distance
 				&& ((CaterpillaRobot) r).number == this.number
--- src/SwivelArmRobot.java
+++ src/SwivelArmRobot.java
@@ -1,27 +1,71 @@
+/**
+ * Class SwivelArmRobot extends the class Robot
+ * 
+ * @author Gruppe222
+ * 
+ */
 public class SwivelArmRobot extends Robot {
 	private int rotations;
 
+	/**
+	 * First Constructor, calls the super constructor
+	 * 
+	 * @param nr
+	 *            the number of the robot
+	 */
 	public SwivelArmRobot(int nr) {
 		super(nr);
 	}
 
+	/**
+	 * Alternative constructor, calls the super constructor
+	 * 
+	 * @param nr
+	 *            the number of the robot
+	 * @param hour
+	 *            the operating hours
+	 */
 	public SwivelArmRobot(int nr, int hour) {
 		super(nr, hour);
 	}
 
+	/**
+	 * Alternative constructor, calls the super constructor for number and
+	 * hours.
+	 * 
+	 * @param nr
+	 *            the number of the robot
+	 * @param hour
+	 *            the operating hours
+	 * @param rot
+	 *            the rotations as integer
+	 */
 	public SwivelArmRobot(int nr, int hour, int rot) {
 		super(nr, hour);
 		this.rotations = rot;
 	}
 
+	/**
+	 * Getter for the rotations
+	 * 
+	 * @return rotations as integer
+	 */
 	public int getRotations() {
 		return this.rotations;
 	}
 
+	/**
+	 * Increments the rotations by 1.
+	 */
 	public void increaseRotations() {
 		this.rotations++;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see Robot#uncheckedEqual(Robot)
+	 */
 	@Override
 	protected boolean uncheckedEqual(Robot r) {
 		return ((SwivelArmRobot) r).rotations == this.rotations
